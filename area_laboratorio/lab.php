bdc7f90d149955ca111522138468ae9f58560f6c
wellerson <wellersondavidv@gmail.com>
2019-10-03 16:22:05 -0300

    update

--- area_laboratorio/lab.php
+++ area_laboratorio/lab.php
@@ -10,7 +10,7 @@ include_once('../banco/sessao.php');
   <script src="../js/phaser/3.12.0/phaser.min.js"></script>
   <script src="../js/phaser/GameScalePlugin.js"></script>
 
-  <script src="../js/jquery.js"></script>
+  <script src="js/vendor/jquery.js"></script>
   <script src="../js/popper.js/1.14.3/popper.min.js"></script>
   <script src="../js/bootstrap/4.1.3/bootstrap.min.js"></script>
 
@@ -35,7 +35,7 @@ include_once('../banco/sessao.php');
 
   <link rel="stylesheet" href="../estilos/bootstrap/4.1.3/bootstrap.min.css">
   <link rel="stylesheet" href="../awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/lab.css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,700,900" rel="stylesheet">
