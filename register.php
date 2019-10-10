34d156c8c87ff6e072440b972a4f67129b1c5ce9
wellerson <wellersondavidv@gmail.com>
2019-10-10 16:03:35 -0300

    update

--- register.php
+++ register.php
@@ -1,5 +1,24 @@
 <?php
 spl_autoload_register(function ($class_name) {
-  require URL_SYSTEM.'classes/'.$class_name . '.class.php';
+
+  $dir_classes = URL_SYSTEM . 'classes/';
+  $diretorios = array('');
+  
+  //procurando diretorios
+  $diretorio = dir($dir_classes);
+  while ($arquivo = $diretorio->read()) {
+    if ($arquivo == '.' || $arquivo == '..')
+      continue;
+    if (is_dir($dir_classes.$arquivo)) {
+      $diretorios[] = $arquivo . '/';
+    }
+  }
+  $diretorio->close();
+
+  foreach ($diretorios as $dir) {
+    $file =  $dir_classes . $dir . $class_name . '.class.php';
+    if (file_exists($file)) {
+      require $file;
+    }
+  }
 });
-?>
