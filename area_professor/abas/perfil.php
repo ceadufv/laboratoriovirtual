f1ea6d2e461b39ad6f8df9b2a101aa1511d52000
tsmarcelo <tsmarcelo@gmail.com>
2019-10-11 16:58:29 -0300

    perfil

--- area_professor/abas/perfil.php
+++ area_professor/abas/perfil.php
@@ -1,42 +1,48 @@
 <?php
-  $objUsuario = new Usuario();
+$objUsuario = new Usuario();
 
-  $dados = $objUsuario->getAlunoEspecifico($_SESSION['id_usuario']);
+$dados = $objUsuario->getAlunoEspecifico($_SESSION['id_usuario']);
 
-  if($_POST["acao"] == 'salvar') {
-
-  }
+if ($_POST["acao"] == 'atualizar') { 
 
 
+  print_r($_POST);
+  die;
+  $objUsuario->atualizarDadosPerfil($_POST);
+}
 ?>
-<script type="text/javascript" src="js/abas/perfil.js"></script>
-<div class="container">
-  <form>
-    <div class="row">
-      <div class="col">  
-        <div class="form-group">
-          <label for="nome">Nome</label>
-          <input type="text" class="form-control" id="nome" placeholder="Digite seu nome...">
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">    
-        <div class="form-control">
-            <label for="senha">Senha</label>
-            <input type="password" class="form-control" id="senha" placeholder="Digite sua senha...">
 
-            <label for="senha2">Digite novamente a senha</label>
-            <input type="password" class="form-control" id="senha2" placeholder="Digite novamente a mesma senha...">
-        </div>    
+<div class="container">
+  <h3>Meu perfil</h3>
+  <p>Aqui é possível atualizar os dados da sua conta.</p>
+  <form id="aluno" name="aluno" method="post" action="<?php echo URL_SITE; ?>area_professor/index.php?aba=perfil&cadastro=ok" enctype="multipart/form-data">
+    <div class="form-row">
+      <div class="form-group col-md-4">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" id="nome" placeholder="Digite seu nome..." value="<?php echo $dados["nome"]?>">
       </div>
     </div>
-    <div class="col">
-    <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail...">
+
+    <div class="form-row">
+      <div class="form-group col-md-4">
+          <label for="email">E-mail</label>
+          <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail..." value="<?php echo $dados["email"]?>">
+      </div>
     </div>
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
-  </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-4">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" id="senha" placeholder="Digite sua senha...">
+      </div>
+      <div class="form-group col-md-4">
+        <label for="senha2">Digite novamente sua senha</label>
+        <input type="password" class="form-control" id="senha2" placeholder="Digite a mesma senha...">
+      </div>
+    </div>
+    <input type="submit" class="btn btn-primary" value="Salvar">
+    <input type="hidden" name="acao" value="atualizar">
+  </form>
 </div>
+
+<script type="text/javascript" src="js/abas/perfil.js"></script>
